b9c6427cce54c4f1f3450f7d971114748bd3961f
juan.tabares <juan.tabares@coytex.com.co>
2022-08-24 22:06:12 -0500

    eliminando imagen al igual que se elimina la publicacion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -87,6 +88,13 @@ class PostController extends Controller
         // ? Debido a PostPolicy solo con la línea de abajo hago lo mismo que arriba
         $this->authorize('delete', $post);
         $post->delete();
+
+        // Eliminar la imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+
+        if (File::exists($imagen_path)){
+            unlink($imagen_path);
+        }
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
