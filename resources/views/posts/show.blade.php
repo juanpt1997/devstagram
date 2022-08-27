37801ff373c44ff7eb0b93da23ebe7b8c1564bd1
juan.tabares <juan.tabares@coytex.com.co>
2022-08-27 11:28:55 -0500

    contador de likes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,9 +8,9 @@
     <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-            <div class="p-3 flex items-center gap-4">
+            <div class="p-3 flex items-center gap-2">
                 @auth
-                    @if($post->checkLike(auth()->user()))
+                    @if ($post->checkLike(auth()->user()))
                         <form method="post" action="{{ route('posts.likes.destroy', $post) }}">
                             @method('DELETE')
                             @csrf
@@ -30,8 +30,8 @@
                             @csrf
                             <div class="my-4">
                                 <button type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5"
-                                        stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                     </svg>
@@ -42,7 +42,10 @@
                     @endif
                 @endauth
 
-                <p>0 likes</p>
+                <p class="font-bold">
+                    {{ $post->likes->count() }}
+                    <span class="font-normal"> Likes</span>
+                </p>
             </div>
 
             <div>
