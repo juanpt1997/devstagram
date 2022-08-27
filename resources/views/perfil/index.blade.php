cc0d9072ac1d16ccc5984605ab98cb1e44f863c8
juan.tabares <juan.tabares@coytex.com.co>
2022-08-27 12:16:01 -0500

    formulario creado para modificar el perfil

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -5,5 +5,30 @@
 @endsection
 
 @section('content')
-    
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form class="mt-10 md:mt-0" action="">
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">Username</label>
+                    <input id="username" name="username" type="text" placeholder="Tu nombre de usuario"
+                        class="border p-3 w-full rounded-lg 
+                    @error('username') border-red-500 @enderror
+                    "
+                        value='{{ auth()->user()->username }}'>
+                    @error('username')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">Imagen Perfil</label>
+                    <input id="imagen" name="imagen" type="file" class="border p-3 w-full rounded-lg"
+                        accept='.jpg, .jpeg, .png'>
+                </div>
+
+                <input type="submit" value="Guardar cambios"
+                    class="bg-sky-500 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
 @endsection
