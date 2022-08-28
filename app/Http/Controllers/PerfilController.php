fa0fa6448c42931c3cab04dcedb0fbf6a4d3fb01
juan.tabares <juan.tabares@coytex.com.co>
2022-08-28 10:06:06 -0500

    Guardando la imagen en el servidor y lo del formulario, hace el campo imagen en la base de datos para colminar la edicion del perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -24,5 +26,30 @@ class PerfilController extends Controller
         $this->validate($request, [
             'username' => ['required', 'unique:users,username,' . auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
         ]);
+
+        if ($request->imagen) {
+            // $input = $request->all();
+            // Capturo el archivo del formulario
+            $imagen = $request->file('imagen');
+            // Nombre único
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+            // La creo como tipo objeto de Image
+            $imagenServidor = Image::make($imagen);
+            // La vuelvo cuadrada
+            $imagenServidor->fit(1000, 1000);
+            // Ruta
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            // Guardo en la ruta
+            $imagenServidor->save($imagenPath);
+        }
+
+        // Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        // Redireccionar
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
