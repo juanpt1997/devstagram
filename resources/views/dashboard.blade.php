dd7a66565d91e5b21c4769e1e02afca812a12ed2
juan.tabares <juan.tabares@coytex.com.co>
2022-09-01 11:37:28 -0500

    modificado  por ->user en dashboard para que sea igual al de home y posterior poder utilizar los componentes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -75,7 +75,7 @@
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($posts as $post)
                     <div>
-                        <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
+                        <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
                             <img src=" {{ asset('uploads') . '/' . $post->imagen }}"
                                 alt="Imagen del post {{ $post->titulo }}">
                         </a>
