c05286d975aeb20584a3035196e7aac19d41439f
juan.tabares <juan.tabares@coytex.com.co>
2022-09-01 10:54:28 -0500

    obtener los usuarios que seguimos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
+        // Obtener a quienes seguimos
+        // pluck sirve para obtener ciertos campos
+        // toArray convierte a un arreglo
+        dd(auth()->user()->following->pluck('id')->toArray());
+
         return view('home');
     }
 }
