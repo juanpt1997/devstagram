40eb528fbf7ab097884d40401682b2a075eb087b
juan.tabares <juan.tabares@coytex.com.co>
2022-09-01 16:41:49 -0500

    volviendo reactivo la interfaz para que el corazon cambie de color apenas da clic

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -7,15 +7,23 @@ use Livewire\Component;
 class LikePost extends Component
 {
     public $post;
+    public $isLiked;
+
+    // ? Funciona como un constructor, se llama apenas se instancia el componente
+    public function mount($post){
+        $this->isLiked = $post->checkLike(auth()->user());
+    }
 
     public function like()
     {
         if ($this->post->checkLike(auth()->user())) {
             $this->post->likes()->where('user_id', auth()->user()->id)->delete();
+            $this->isLiked = false;
         } else {
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
+            $this->isLiked = true;
         }
     }
 
