2f57d460696a6f130e83a174d7abf788694f4f92
juan.tabares <juan.tabares@coytex.com.co>
2022-09-01 10:58:18 -0500

    obtener posts de los usuarios que seguimos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,8 +12,11 @@ class HomeController extends Controller
         // Obtener a quienes seguimos
         // pluck sirve para obtener ciertos campos
         // toArray convierte a un arreglo
-        dd(auth()->user()->following->pluck('id')->toArray());
+        $ids = auth()->user()->following->pluck('id')->toArray();
+        $posts = Post::whereIn('user_id', $ids)->paginate(20);
 
-        return view('home');
+        return view('home', [
+            'posts' => $posts
+        ]);
     }
 }
