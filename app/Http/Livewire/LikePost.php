a90ec0da4d4c1df5bf5f2a12dad28fc0c82bb295
juan.tabares <juan.tabares@coytex.com.co>
2022-09-01 16:12:37 -0500

    Agregando evento de livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -8,6 +8,11 @@ class LikePost extends Component
 {
     public $post;
 
+    public function like()
+    {
+        return "desde la fn de like";
+    }
+
     public function render()
     {
         return view('livewire.like-post');
